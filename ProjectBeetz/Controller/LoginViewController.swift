e771420956dd7b248bc040aeffcaa9b69e8f643f
Aaron228 <intp81@gmail.com>
2019-03-14 18:09:48 -0400

    Updated with merge between master and master1.

--- ProjectBeetz/Controller/LoginViewController.swift
+++ ProjectBeetz/Controller/LoginViewController.swift
@@ -45,7 +45,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         //Guard statements are what check the textfields to make sure they contain something.
         
         guard let userEmail = emailAddressTextField.text, !userEmail.isEmpty else {
-            self.showMessage(messageToDisplay: "Not so fast. You're gonna need put in an email address.")
+            self.showMessage(messageToDisplay: "Not so super fast. You're gonna need put in an email address.")
             return
         }
         
